0cfe8c68d3452537e40f701a73ed1afeb4f6201f
Yolino <yolino23@gmail.com>
2025-10-21 12:18:02 +0200

    Lares #7 #8 #9 Ajout de GET POST PUT DELETE afin de mieux gérer les serrures et les groupes de serrures

--- backend/locks/urls.py
+++ backend/locks/urls.py
@@ -1,6 +1,18 @@
 from django.urls import path
-from .views import LocksView
+from .views import (
+    LocksView,
+    LockGroupsView,
+    AddLocksToGroupView,
+    GroupLocksView,
+    RemoveLockFromGroupView,
+    DeleteLockGroupView
+)
 
 urlpatterns = [
-    path('locks/', LocksView.as_view(), name='locks'),
+    path('', LocksView.as_view(), name='locks'),
+    path('lock-groups/', LockGroupsView.as_view(), name='lock-groups'),
+    path('lock-groups/<int:group_id>/add-locks/', AddLocksToGroupView.as_view(), name='add-locks-to-group'),
+    path('lock-groups/<int:group_id>/locks/', GroupLocksView.as_view(), name='group-locks'),
+    path('lock-groups/<int:group_id>/remove-locks/', RemoveLockFromGroupView.as_view(), name='remove-locks-from-group'),
+    path('lock-groups/<int:group_id>/delete/', DeleteLockGroupView.as_view(), name='delete-lock-group'),
 ]
