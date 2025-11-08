2455d0e225a759d46575c64b51dbd967ba73dc4e
Sleeeee <jstasjustin@gmail.com>
2025-11-08 17:34:01 +0100

    LARES #31 Ajout du model Django des codes de badges RFID

--- backend/users/models.py
+++ backend/users/models.py
@@ -7,7 +7,8 @@ User = get_user_model()
 
 class UserKeypadCode(models.Model):
     user = models.OneToOneField(
-        User, on_delete=models.CASCADE, related_name='keypad_codes')
+        User, on_delete=models.CASCADE, related_name='keypad_codes'
+    )
     code_hash = models.CharField(max_length=128)
 
     def set_code(self, raw_code):
@@ -23,3 +24,24 @@ class UserKeypadCode(models.Model):
 
     def __str__(self):
         return f"{self.user}'s keypad code"
+
+
+class UserBadgeCode(models.Model):
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, related_name='badge_codes'
+    )
+    code_hash = models.CharField(max_length=128)
+
+    def set_code(self, raw_code):
+        self.code_hash = make_password(raw_code)
+
+    def check_code(self, raw_code):
+        return check_password(raw_code, self.code_hash)
+
+    def save(self, *args, **kwargs):
+        if not self.code_hash.startswith("pbkdf2_"):
+            self.code_hash = make_password(self.code_hash)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.user}'s badge code"
